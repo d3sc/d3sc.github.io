f62dcfbbec35f8fc8adcea727849372e754f408e
Ikbar <ikbar686@gmail.com>
2023-08-01 16:59:59 +0700

    fix: video-embeded

--- src/components/HeroProject.jsx
+++ src/components/HeroProject.jsx
@@ -13,19 +13,21 @@ export default function HeroProject() {
         </div>
         <div className="w-full px-4">
           <div className="flex flex-wrap items-center justify-center gap-10">
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/9pyZO3N_ltE"
-              title="YouTube video player"
-              frameBorder="0"
-              allowFullScreen></iframe>
-            <div className="w-1/2  ">
+            <video controls width="560" height="315">
+              <source src="https://drive.google.com/uc?id=1xhScxitJaFI71xM4tsd0KRkl6TPupRyF&export=download" />
+            </video>
+            <div className="lg:w-1/2">
               <h2 className="font-bold text-white text-3xl mb-4 sm:text-4xl lg:text-5xl">Samurai Game</h2>
               <p className="font-medium text-base text-secondary md:text-lg">
                 I am a beginner game maker, recently I made a samurai themed game, in this game we can attack, jump, run, and shoot shuriken. in the
                 game there are enemies that we can attack and kill . I make this game with Godot 4.
               </p>
+              <br />
+              <a
+                href="https://youtu.be/9pyZO3N_ltE"
+                className="text-base capitalize font-semibold text-white bg-primary py-3 px-8 rounded-md hover:shadow-lg hover:opacity-80 transition-all duration-300 ease-in-out mt-10">
+                see more
+              </a>
             </div>
           </div>
         </div>
